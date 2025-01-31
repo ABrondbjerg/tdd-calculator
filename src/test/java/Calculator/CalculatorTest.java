8a6cf70fe7ffd83bb7c29b072c2d7bbae98f807e
Alex Brøndbjerg <alex.brondbjerg@persgroep.net>
2025-01-31 09:12:47 +0100

    Files to see Git works

---
+++ src/test/java/Calculator/CalculatorTest.java
@@ -0,0 +1,49 @@
+package Calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    Calculator instance = new Calculator();
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testAdd(){
+        int expected = 5;
+        int actual = instance.add(2,4);
+        assertEquals(expected,actual);
+    }
+    @Test
+    void testSubtract(){
+        int expected = 0;
+        int actual = instance.subtract(4,4);
+        assertEquals(expected,actual);
+    }
+    @Test
+    void testMultiply(){
+        int expected = 10;
+        int actual = instance.multiply(5,2);
+        assertEquals(expected,actual);
+    }
+    @Test
+    void testDivide(){
+        int expected = 5;
+        int actual = instance.divide(10,2);
+        assertEquals(expected,actual);
+    }
+
+
+
+
+}
